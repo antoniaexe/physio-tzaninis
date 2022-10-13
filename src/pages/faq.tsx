3c12fa93ea61ffa487aa12d8f3a0b2fbd32eb2d2
Antonia Exertzi <antoniaexe@gmail.com>
2022-10-13 23:37:27 +0300

    refactor questions to a new componnt and add a title in faq page

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,20 +1,24 @@
 import { Background } from '../background/Background';
 import { Section } from '../layout/Section';
 import { Header } from '../templates/Header';
-
-const faq = () => {
+const Question = ({children}:any)=>{
+  return <dt className="font-medium hover:font-bold">{children}</dt>}
+const Faq = () => {
   return (
     <Background color="bg-gray-100">
       <Header />
+    
       <Section yPadding="pt-20 pb-32">
-        <dl>
-          <dt>Πόσο κοστίζει η συνεδρία;</dt>
+        
+        <h1 className="text-2xl mb-5">Συχνά μας ρωτάτε:</h1>
+        <dl className='pl-3'>
+          <Question>Πόσο κοστίζει η συνεδρία;</Question>
           <dd>100000 ευρώ</dd>
-          <dt>Πρέπει να έχω κάποιο εξοπλισμό στο σπίτι μου;</dt>
+          <Question>Πρέπει να έχω κάποιο εξοπλισμό στο σπίτι μου;</Question>
           <dd>Όχι παρέχουμε εμείς ότι εξοπλισμό ενδέχεται να χρειαστεί</dd>
         </dl>
       </Section>
     </Background>
   );
 };
-export default faq;
+export default Faq;
