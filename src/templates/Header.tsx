9f07014ba1295cbdad452e521d4e5a4037e75e59
Antonia Exertzi <antoniaexe@gmail.com>
2022-10-13 23:41:55 +0300

    update links in header

--- src/templates/Header.tsx
+++ src/templates/Header.tsx
@@ -3,14 +3,20 @@ import Link from 'next/link';
 import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
-
+/**
+ * 
+ * About Us
+What We Treat
+How We Treat
+Contact Us
+ */
 export function Header() {
   return (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>GitHub</a>
+          <Link href="/faq">
+            <a>Faq</a>
           </Link>
         </li>
         <li>
