0664093c44143c34f9b190da96452d6cb8263877
Antonia Exertzi <antoniaexe@gmail.com>
2022-11-09 13:25:58 +0200

    Make faq page prettier

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,17 +1,17 @@
 import { Background } from '../background/Background';
 import { Section } from '../layout/Section';
 import { Header } from '../templates/Header';
-const Question = ({children}:any)=>{
-  return <dt className="font-medium hover:font-bold">{children}</dt>}
+
+const Question = ({ children }: any) => {
+  return <dt className="font-medium hover:font-bold">{children}</dt>;
+};
 const Faq = () => {
   return (
     <Background color="bg-gray-100">
       <Header />
-    
       <Section yPadding="pt-20 pb-32">
-        
         <h1 className="text-2xl mb-5">Συχνά μας ρωτάτε:</h1>
-        <dl className='pl-3'>
+        <dl className="pl-3">
           <Question>Πόσο κοστίζει η συνεδρία;</Question>
           <dd>100000 ευρώ</dd>
           <Question>Πρέπει να έχω κάποιο εξοπλισμό στο σπίτι μου;</Question>
