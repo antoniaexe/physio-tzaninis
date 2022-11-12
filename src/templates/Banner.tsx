715ad55e649ec89eeec61bb7f8d224c65fec59f4
Antonia Exertzi <antoniaexe@gmail.com>
2022-11-12 14:05:25 +0200

    Change Button import to '@material-tailwind/react'

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -1,6 +1,6 @@
+import { Button } from '@material-tailwind/react';
 import Link from 'next/link';
 
-import { Button } from '../button/Button';
 import { CTABanner } from '../cta/CTABanner';
 import { Section } from '../layout/Section';
 
