784ad25f654a137694eeac6361f1a6a441ecfff6
Antonia Exertzi <antoniaexe@gmail.com>
2022-11-12 14:01:17 +0200

    Change navbar font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = withMT({
       sm: '0.875rem',
       base: '1rem',
       lg: '1.125rem',
-      xl: '1.25rem',
+      xl: '1.20rem',
       '2xl': '1.5rem',
       '3xl': '1.875rem',
       '4xl': '2.25rem',
