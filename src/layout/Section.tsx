b2c8bd04542126fb1d3479e2f10accac891cc94a
Antonia Exertzi <antoniaexe@gmail.com>
2022-11-15 10:23:14 +0200

    Add text in VerticalFeatures and change text color

--- src/layout/Section.tsx
+++ src/layout/Section.tsx
@@ -9,14 +9,14 @@ type ISectionProps = {
 
 const Section = (props: ISectionProps) => (
   <div
-    className={`max-w-screen-lg mx-auto px-3 ${
+    className={`max-w-screen-lg mx-auto px-3${
       props.yPadding ? props.yPadding : 'py-16'
     }`}
   >
     {(props.title || props.description) && (
       <div className="mb-12 text-center">
         {props.title && (
-          <h2 className="text-4xl text-gray-900 font-bold">{props.title}</h2>
+          <h2 className="text-4xl text-indigo-600 font-bold">{props.title}</h2>
         )}
         {props.description && (
           <div className="mt-4 text-xl md:px-20">{props.description}</div>
--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -2,13 +2,20 @@ import { Button } from '@material-tailwind/react';
 
 import { VerticalFeatureRow } from '../feature/VerticalFeatureRow';
 import { BackPainIcon } from '../images/BackpainIcon';
+import { KneepainIcon } from '../images/KneepainIcon';
+import { PostOpIcon } from '../images/PostOpIcon';
 import { ShoulderPainIcon } from '../images/ShoulderPainIcon';
+import { SportsIjuriesIcon } from '../images/SportsInjuriesIcon';
 import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
   <Section
-    title="Your title here"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
+    title="Get rid of your pain!"
+    description="Kaizen Physical Therapy clinic focuses on practicing the best techniques 
+    to treat the root cause of pain while cultivating long-lasting relationships with our 
+    patients. Our Seattle, WA physical therapists are excited to see you achieve your health, 
+    wellness, and fitness goals. We are committed to assisting you in living a pain-free life 
+    again!"
   >
     <div className="iconlist">
       <div className="icon">
@@ -43,7 +50,7 @@ const VerticalFeatures = () => (
         <a>
           <div className="pb-4">
             <Button color="white">
-              <BackPainIcon />
+              <PostOpIcon />
             </Button>
           </div>
           <div>
@@ -55,7 +62,7 @@ const VerticalFeatures = () => (
         <a>
           <div className="pb-4">
             <Button color="white">
-              <BackPainIcon />
+              <KneepainIcon />
             </Button>
           </div>
           <div>
@@ -67,7 +74,7 @@ const VerticalFeatures = () => (
         <a>
           <div className="pb-4">
             <Button color="white">
-              <BackPainIcon />
+              <SportsIjuriesIcon />
             </Button>
           </div>
           <div>
