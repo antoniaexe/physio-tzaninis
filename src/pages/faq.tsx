1ef3fa673f65b6ca8e0c54c2b82ae9cab7a29f01
Antonia Exertzi <antoniaexe@gmail.com>
2022-11-23 21:50:35 +0200

    Add accordion feature in faq page

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,25 +1,58 @@
-import { Background } from '../background/Background';
+import { Fragment, SetStateAction, useState } from 'react';
+
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from '@material-tailwind/react';
+
 import { Section } from '../layout/Section';
 import { Base } from '../templates/Base';
 
-const Question = ({ children }: any) => {
-  return <dt className="font-medium hover:font-bold">{children}</dt>;
-};
-const Faq = () => {
+export default function Faq() {
+  const [open, setOpen] = useState(1);
+
+  const handleOpen = (value: SetStateAction<number>) => {
+    setOpen(open === value ? 0 : value);
+  };
+
   return (
     <Base>
-      <Background color="bg-gray-100">
-        <Section yPadding="pt-20 pb-32">
-          <h1 className="text-2xl mb-5">Συχνά μας ρωτάτε:</h1>
-          <dl className="pl-3">
-            <Question>Πόσο κοστίζει η συνεδρία;</Question>
-            <dd>100000 ευρώ</dd>
-            <Question>Πρέπει να έχω κάποιο εξοπλισμό στο σπίτι μου;</Question>
-            <dd>Όχι παρέχουμε εμείς ότι εξοπλισμό ενδέχεται να χρειαστεί</dd>
-          </dl>
-        </Section>
-      </Background>
+      <Section yPadding="pt-20 pb-32 mt-20">
+        <h1 className="text-2xl mb-20 text-indigo-600">Συχνά μας ρωτάτε</h1>
+        <Fragment>
+          <Accordion open={open === 1}>
+            <AccordionHeader onClick={() => handleOpen(1)}>
+              What is Material Tailwind?
+            </AccordionHeader>
+            <AccordionBody>
+              We're not always in the position that we want to be at. We're
+              constantly growing. We're constantly making mistakes. We're
+              constantly trying to express ourselves and actualize our dreams.
+            </AccordionBody>
+          </Accordion>
+          <Accordion open={open === 2}>
+            <AccordionHeader onClick={() => handleOpen(2)}>
+              How to use Material Tailwind?
+            </AccordionHeader>
+            <AccordionBody>
+              We're not always in the position that we want to be at. We're
+              constantly growing. We're constantly making mistakes. We're
+              constantly trying to express ourselves and actualize our dreams.
+            </AccordionBody>
+          </Accordion>
+          <Accordion open={open === 3}>
+            <AccordionHeader onClick={() => handleOpen(3)}>
+              What can I do with Material Tailwind?
+            </AccordionHeader>
+            <AccordionBody>
+              We're not always in the position that we want to be at. We're
+              constantly growing. We're constantly making mistakes. We're
+              constantly trying to express ourselves and actualize our dreams.
+            </AccordionBody>
+          </Accordion>
+        </Fragment>
+      </Section>
     </Base>
   );
-};
-export default Faq;
+}
