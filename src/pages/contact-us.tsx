128dcad32d4869f789d5aa9bd5dfbc4b24254ffa
Antonia Exertzi <antoniaexe@gmail.com>
2022-11-23 21:50:01 +0200

    Creat form for contact

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -10,17 +10,35 @@ import {
 } from '@material-tailwind/react';
 
 import { Section } from '../layout/Section';
+import { createContactForm } from '../lib';
 import { Base } from '../templates/Base';
 
 const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
   e.preventDefault();
   console.log(e.target);
+  // @ts-ignore
+  const fullname = e.target.elements.fullname.value;
+  // @ts-ignore
+  const email = e.target.elements.email.value;
+  // @ts-ignore
+  const message = e.target.elements.message.value;
+  console.log(fullname, email, message);
+  createContactForm({ fullname, email, message });
+  // @ts-ignore
+  e.target.elements.fullname.value = '';
+  // @ts-ignore
+  e.target.elements.email.value = '';
+  // @ts-ignore
+  e.target.elements.message.value = '';
 };
+
 const ContactUs = () => {
   return (
     <Base>
       <Section yPadding="pt-20 pb-32 mt-20">
-        <h1 className="text-2xl mb-20">Επικοινωνήστε μαζί μας</h1>
+        <h1 className="text-2xl mb-20 text-indigo-600">
+          Επικοινωνήστε μαζί μας
+        </h1>
         <form onSubmit={handleSubmit}>
           <div className="flex flex-col gap-9">
             <div className="flex w-full items-end gap-4">
@@ -42,7 +60,7 @@ const ContactUs = () => {
             <div>
               <Popover>
                 <PopoverHandler>
-                  <Button variant="gradient" type="submit">
+                  <Button className="bg-indigo-600" type="submit">
                     Αποστολή
                   </Button>
                 </PopoverHandler>
